bf6f2682e0c503bb96afcf75bbdf073755fa45d2
root <root@cat022.dcatalin.ro>
2025-06-08 16:26:53 +0300

    small refinements: got rid of redundant PHP middle code

--- App/views/books/create.view.php
+++ App/views/books/create.view.php
@@ -4,38 +4,41 @@
 
 <?= loadPartial('top-banner') ?>
 
-<!-- Post a Job Form Box -->
+<!-- Post a Book Form Box -->
     <section class="flex justify-center items-center mt-20">
       <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
+        <h2 class="text-4xl text-center font-bold mb-4">Create a Book Presentation</h2>
         <!-- <div class="message bg-red-100 p-3 my-3">This is an error message.</div>
         <div class="message bg-green-100 p-3 my-3">
           This is a success message.
         </div> -->
         <form method="POST">
+
+          <br />
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
-            Job Info
+            Book Info
           </h2>
+
           <div class="mb-4">
             <input
               type="text"
               name="title"
-              placeholder="Job Title"
+              placeholder="Book Title"
               class="w-full px-4 py-2 border rounded focus:outline-none"
             />
           </div>
           <div class="mb-4">
             <textarea
               name="description"
-              placeholder="Job Description"
+              placeholder="Book Description"
               class="w-full px-4 py-2 border rounded focus:outline-none"
             ></textarea>
           </div>
           <div class="mb-4">
             <input
               type="text"
-              name="salary"
-              placeholder="Annual Salary"
+              name="year"
+              placeholder="Release Year"
               class="w-full px-4 py-2 border rounded focus:outline-none"
             />
           </div>
@@ -43,7 +46,7 @@
             <input
               type="text"
               name="requirements"
-              placeholder="Requirements"
+              placeholder="ISBN"
               class="w-full px-4 py-2 border rounded focus:outline-none"
             />
           </div>
@@ -51,13 +54,16 @@
             <input
               type="text"
               name="benefits"
-              placeholder="Benefits"
+              placeholder="Genre"
               class="w-full px-4 py-2 border rounded focus:outline-none"
             />
           </div>
+
+          <br />
           <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
-            Company Info & Location
+            Publisher Info & Location
           </h2>
+
           <div class="mb-4">
             <input
               type="text"
