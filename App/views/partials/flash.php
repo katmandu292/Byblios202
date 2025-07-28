764ce6e0d26e15f31a72b27eb76674104513faac
katmandu292 <catman_du@dcatalin.ro>
2025-07-28 20:38:08 +0300

    Flash messages are refactored. Lecture 126 in progress

--- App/views/partials/flash.php
+++ App/views/partials/flash.php
@@ -1,12 +1,15 @@
 <?php
-  if (isset($_SESSION['success_message'])) {
+
+  $successMessage = Session::getFlashMessage('success_message');
+  $errorMessage = Session::getFlashMessage('error_message');
+
+  if ($successMessage !== null) {
      echo '<div class="message bg-green-100 p-3 my-3">';
      echo $_SESSION['success_message'];
      echo '</div>';
-     unset($_SESSION['success_message']);
   }
 
-  if (isset($_SESSION['error_message'])) {
+  if ($errorMessage !== null) {
      echo '<div class="message bg-red-100 p-3 my-3">';
      echo $_SESSION['error_message'];
      echo '</div>';
